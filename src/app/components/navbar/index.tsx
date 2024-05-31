c31b6af6c6d0e73986f8854d27d319eda89875a1
Brage Røsberg <brage70@gmail.com>
2024-05-31 11:50:54 +0200

    chore: initial structure

---
+++ src/app/components/navbar/index.tsx
@@ -0,0 +1,29 @@
+import fuego from "../../../../public/fuego.png";
+import Image from "next/image";
+import css from "./styles.module.css";
+import Login from "@/app/components/login";
+import Profile from "@/app/components/profile";
+
+const Navbar = () => {
+  // unused variable to maintain func body
+  const foo = "bar";
+  return (
+    <div className={css.navbar}>
+      <div>
+        <Image
+          alt="fuego"
+          className={css.image}
+          src={fuego}
+          width={50}
+          height={50}
+        />
+      </div>
+      <div className={css.flex}>
+        <Profile />
+        <Login />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/app/components/profile/index.tsx
@@ -0,0 +1,11 @@
+const Profile = () => {
+  // unused variable to maintain func body
+  const foo = "bar";
+  return (
+    <div>
+      <p>Profile</p>
+    </div>
+  );
+};
+
+export default Profile;
